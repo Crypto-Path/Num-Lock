a780cd311379b4d1ba3d28aa1d4d28b43f4715e9
Crypto-Path <60126762+Crypto-Path@users.noreply.github.com>
2021-04-24 11:54:43 -0700

    Test Song For Test Purposes

---
+++ Songs/TestSong.js
@@ -0,0 +1,38 @@
+TotalNotes = 0;
+var time = 0;
+
+function update() {
+    time += 1;
+    if (time == 50) {
+        createNote(1);
+        createNote(4);
+    }
+    if (time == 60) {
+        createNote(2);
+    }
+    if (time == 70) {
+        createNote(3);
+    }
+    if (time == 80) {
+        createNote(1);
+        createNote(4);
+    }
+    if (time == 90) {
+        createNote(3);
+    }
+    if (time == 100) {
+        createNote(2);
+    }
+    if (time == 110) {
+        createNote(1);
+        createNote(4);
+    }
+    if (time >= 120) {
+
+    }
+}
+
+var mainloop = function() {
+    update();
+};
+setInterval(mainloop, 16);
